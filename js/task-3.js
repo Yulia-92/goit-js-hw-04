92b94e4342fdca42632f7f1de910b8fd5a7b9569
Yulia <borisovayulia2013@gmail.com>
2025-01-10 21:29:21 +0200

    html

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,15 @@
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      filteredNumbers.push(numbers[i]);
-    }
-  }
-  return filteredNumbers;
-}
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.level = Math.floor(profile.playTime / 100);
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
